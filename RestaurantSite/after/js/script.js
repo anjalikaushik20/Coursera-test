b814d687f5304ff792f53a217fea52042628b8b6
Anjali Kaushik <anjali.kaushik.209@gmail.com>
2020-08-02 16:52:13 +0530

    js added

---
+++ RestaurantSite/after/js/script.js
@@ -0,0 +1,8 @@
+$(function () {
+    $("#navbar-toggler").blur(function (event) {
+      var screenWidth = window.innerWidth;
+      if (screenWidth < 768) {
+        $("#collapsable-nav").collapse('hide');
+      }
+    });
+  });
